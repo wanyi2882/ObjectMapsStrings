37c5f786004de32f6032f590d1a515e61be22df9
wanyi2882 <wanyi.2882@gmail.com>
2022-01-14 09:27:30 +0800

    Songs and Student example

--- com/company/Song.java
+++ com/company/Song.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.*;
+
 public class Song {
     private String typeList;
     private String name;
@@ -23,5 +25,40 @@ public class Song {
         return this.time;
     }
 
+    // solution for Songs
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        List<Song> songs = new ArrayList<>();
+
+        int n = Integer.parseInt(sc.nextLine());
+
+        for(int i = 0; i < n; i++){
+            String input = sc.nextLine();
+
+            String[] data = input.split("_");
+            String typeList = data[0];
+            String name = data[1];
+            String time = data[2];
+
+            Song song = new Song(typeList, name, time);
+
+            songs.add(song);
+        }
+
+        String finalCommand = sc.nextLine();
+
+        if(finalCommand.equals("all")){
+            for(Song song : songs){
+                System.out.println(song.getName());
+            }
+        }
+        else{
+            for(Song song : songs){
+                if(song.getTypeList().equals(finalCommand)){
+                    System.out.println(song.getName());
+                }
+            }
+        }
+    }
 }
--- com/company/Student.java
+++ com/company/Student.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.*;
+
 public class Student {
     private String firstName;
     private String lastName;
@@ -35,5 +37,54 @@ public class Student {
 
     public void setHometown(String hometown) {
         this.hometown = hometown;
+    }
+
+//     Solution for student and student 2.0
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        List<Student> students = new ArrayList<>();
+
+        String input = sc.nextLine();
+
+        while(!"end".equals(input)){
+            String[] data = input.split(" ");
+            String firstName = data[0];
+            String lastName = data[1];
+            int age = Integer.parseInt(data[2]);
+            String hometown = data[3];
+            Student student = new Student(firstName, lastName, age, hometown);
+
+            Student existingStudent = getExistingStudent(student, students);
+
+            if(existingStudent != null){
+                existingStudent.setAge(age);
+                existingStudent.setHometown(hometown);
+            } else {
+                students.add(student);
+            }
+
+
+            input = sc.nextLine();
+        }
+
+        String hometown = sc.nextLine();
+
+        for(Student student: students){
+            if(hometown.equals(student.getHometown())){
+                System.out.printf("%s %s is %d years old.%n", student.getFirstName(), student.getLastName(), student.getAge());
+            }
+        }
+    }
+
+    private static Student getExistingStudent(Student studentToAdd, List<Student> students){
+        for(Student savedStudent : students){
+            if(savedStudent.getFirstName().equals(studentToAdd.getFirstName()) &&
+            savedStudent.getLastName().equals(studentToAdd.getLastName())){
+                return savedStudent;
+            }
+        }
+        return null;
     }
 }
