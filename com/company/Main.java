a241259ba3d9bdd251762354254a2253b44accf9
wanyi2882 <wanyi.2882@gmail.com>
2022-01-19 10:03:34 +0800

    commit

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,93 +1,22 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    // Solution for student and student 2.0
-//    public static void main(String[] args) {
-//	// write your code here
-//        Scanner sc = new Scanner(System.in);
-//
-//        List<Student> students = new ArrayList<>();
-//
-//        String input = sc.nextLine();
-//
-//        while(!"end".equals(input)){
-//            String[] data = input.split(" ");
-//            String firstName = data[0];
-//            String lastName = data[1];
-//            int age = Integer.parseInt(data[2]);
-//            String hometown = data[3];
-//            Student student = new Student(firstName, lastName, age, hometown);
-//
-//            Student existingStudent = getExistingStudent(student, students);
-//
-//            if(existingStudent != null){
-//                existingStudent.setAge(age);
-//                existingStudent.setHometown(hometown);
-//            } else {
-//                students.add(student);
-//            }
-//
-//
-//            input = sc.nextLine();
-//        }
-//
-//        String hometown = sc.nextLine();
-//
-//        for(Student student: students){
-//            if(hometown.equals(student.getHometown())){
-//                System.out.printf("%s %s is %d years old.%n", student.getFirstName(), student.getLastName(), student.getAge());
-//            }
-//        }
-//    }
-//
-//    private static Student getExistingStudent(Student studentToAdd, List<Student> students){
-//        for(Student savedStudent : students){
-//            if(savedStudent.getFirstName().equals(studentToAdd.getFirstName()) &&
-//            savedStudent.getLastName().equals(studentToAdd.getLastName())){
-//                return savedStudent;
-//            }
-//        }
-//        return null;
-//    }
-
-    // solution for Songs
     public static void main(String[] args) {
+        // Write your code here
         Scanner sc = new Scanner(System.in);
 
-        List<Song> songs = new ArrayList<>();
+        String[] stringArray = sc.nextLine().split(" ");
 
-        int n = Integer.parseInt(sc.nextLine());
+        String newString = "";
 
-        for(int i = 0; i < n; i++){
-            String input = sc.nextLine();
-
-            String[] data = input.split("_");
-            String typeList = data[0];
-            String name = data[1];
-            String time = data[2];
-
-            Song song = new Song(typeList, name, time);
-
-            songs.add(song);
-        }
-
-        String finalCommand = sc.nextLine();
-
-        if(finalCommand.equals("all")){
-            for(Song song : songs){
-                System.out.println(song.getName());
+        for(int i = 0; i < stringArray.length; i++){
+            for(int j = 0; j < stringArray[i].length();j ++){
+                newString +=stringArray[i];
             }
         }
-        else{
-            for(Song song : songs){
-                if(song.getTypeList().equals(finalCommand)){
-                    System.out.println(song.getName());
-                }
-            }
-        }
+
+        System.out.println(newString);
     }
 }
